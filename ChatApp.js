07468dbd19a83e2d0d086ffe3c322926d571cddc
Raghvendra <rannyshukla762@gmail.com>
2022-07-28 20:01:16 +0530

    this is first commit

---
+++ ChatApp.js
@@ -0,0 +1,43 @@
+const socket=io("http://localhost:8000");
+
+const form=document.getElementById("send-container");
+const messageInput=document.getElementById("messageInput");
+const messageContainer=document.querySelector(".container");
+
+var audio=new Audio("ting.mp3");
+
+const append=(message,position)=>{
+    const messageElement=document.createElement("div");
+    messageElement.innerText=message;
+    messageElement.classList.add("message");
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position=="left"){
+        audio.play();
+    }
+}
+
+const Name=prompt("Enter your name to join");
+socket.emit("new-user-joined", Name);
+
+socket.on("user-joined",Name=>{
+    append(`${Name} joined the chat`,"right");
+});
+
+socket.on("receive",data=>{
+    append(`${data.Name}: ${data.message}`,"left");
+});
+
+socket.on("left",Name=>{
+    append(`${Name} left the chat`,"right");
+});
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const message=messageInput.value;
+    append(`You: ${message}`,"right");
+    socket.emit("send",message);
+    messageInput.value="";
+});
+
+
---
+++ nodejs/index.js
@@ -0,0 +1,27 @@
+// const { Socket } = require("socket.io");
+
+console.log("This is index.js");
+const io = require("socket.io")(8000,{
+    cors:{
+        origin:"*",
+    }
+});
+
+const users={};
+
+io.on("connection",socket=>{
+    socket.on("new-user-joined",Name=>{
+        console.log("new user",Name);
+        users[socket.id]=Name;
+        socket.broadcast.emit("user-joined",Name);
+    });
+
+    socket.on("send",message=>{
+        socket.broadcast.emit("receive",{message: message, Name: users[socket.id]});
+    });
+
+    socket.on("disconnect",message=>{
+        socket.broadcast.emit("left",users[socket.id]);
+        delete users[socket.id];
+    });
+});
